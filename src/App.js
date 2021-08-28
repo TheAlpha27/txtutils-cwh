f7f1560fc836c69311f9696471245e4c4c8015cf
TheAlpha27 <utsav.soni.27@gmail.com>
2021-08-29 02:09:22 +0530

    Added remove space and copy functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import About from './Components/About';
+// import About from './Components/About';
 import Navbar from './Components/Navbar.js';
-// import TextForm from './Components/TextForm';
+import TextForm from './Components/TextForm';
 
 function App() {
   return (
     <>
     {/* <Navbar title="TextUtils" aboutText="About Us" /> */}
       <Navbar title="TextUtils" />
-      {/* <TextForm /> */}
-      <About/>
+      <TextForm />
+      {/* <About/> */}
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -2,33 +2,44 @@ import React from 'react'
 import { useState } from 'react'
 export default function TextForm() {
     const [text, setText] = useState("");
-    const handleOnChange = (event)=>{
+    const handleOnChange = (event) => {
         setText(event.target.value);
     }
-    const handleUpClick =()=>{
+    const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
     }
-    const handleLoClick =()=>{
+    const handleLoClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
     }
+    const handleCopy = () => {
+        let text = document.getElementById("area");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const handleExtraSpaces = () =>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
     return (
         <>
-        <div className="container">
-            <h1 className="my-2">Enter text for analysis</h1>
-            <div className="mb-3 my-2">
-                <textarea value={text} onChange={handleOnChange} className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+            <div className="container">
+                <h1 className="my-2">Enter text for analysis</h1>
+                <div className="mb-3 my-2">
+                    <textarea value={text} onChange={handleOnChange} className="form-control" id="area" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>To Upper Case</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>To Lower Case</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy text</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             </div>
-            <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>To Upper Case</button>
-            <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>To Lower Case</button>
-        </div>
-        <div className="container">
-        <h2>Number of words: </h2>
-        {text.split(' ').length}
-        <h2>Number of characters: </h2>
-        {text.length}
-        </div>
+            <div className="container">
+                <h2>Number of words: </h2>
+                {text.split(' ').length}
+                <h2>Number of characters: </h2>
+                {text.length}
+            </div>
         </>
     )
 }
