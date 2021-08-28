e12958af303e71188c3afdee73aec2c484e5288b
TheAlpha27 <utsav.soni.27@gmail.com>
2021-08-29 00:06:14 +0530

    added lower case button and few other features

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 export default function TextForm() {
-    const [text, setText] = useState("Enter text here");
+    const [text, setText] = useState("");
     const handleOnChange = (event)=>{
         setText(event.target.value);
     }
@@ -9,13 +9,26 @@ export default function TextForm() {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick =()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     return (
+        <>
         <div className="container">
             <h1 className="my-2">Enter text for analysis</h1>
             <div className="mb-3 my-2">
                 <textarea value={text} onChange={handleOnChange} className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>To Upper Case</button>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>To Upper Case</button>
+            <button className="btn btn-primary mx-1" onClick={handleLoClick}>To Lower Case</button>
         </div>
+        <div className="container">
+        <h2>Number of words: </h2>
+        {text.split(' ').length}
+        <h2>Number of characters: </h2>
+        {text.length}
+        </div>
+        </>
     )
 }
