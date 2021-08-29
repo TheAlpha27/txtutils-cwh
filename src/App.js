f69bd262233cdb36b45bb7694b5cef7e401e6db2
TheAlpha27 <utsav.soni.27@gmail.com>
2021-08-29 21:47:54 +0530

    added dark mode to app.js with integration to other components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 // import About from './Components/About';
 import Navbar from './Components/Navbar.js';
 import TextForm from './Components/TextForm';
+import { useState } from 'react'
+
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor=`#01203f`;
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor=`#d1d2d3`;
+    }
+  }
   return (
     <>
-    {/* <Navbar title="TextUtils" aboutText="About Us" /> */}
-      <Navbar title="TextUtils" />
-      <TextForm />
+      {/* <Navbar title="TextUtils" aboutText="About Us" /> */}
+      <Navbar title="TextUtils" toggleMode = {toggleMode} mode={mode}/>
+      <TextForm mode={mode}/>
       {/* <About/> */}
     </>
   );
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
-export default function TextForm() {
+import PropTypes from 'prop-types'
+
+export default function TextForm(props) {
     const [text, setText] = useState("");
     const handleOnChange = (event) => {
         setText(event.target.value);
@@ -22,19 +24,23 @@ export default function TextForm() {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
     }
+    const handleClear = () =>{
+        setText(" ");
+    }
     return (
         <>
-            <div className="container">
+            <div className="container" style={{color: props.mode==='light'?'black':'white'}}>
                 <h1 className="my-2">Enter text for analysis</h1>
                 <div className="mb-3 my-2">
-                    <textarea value={text} onChange={handleOnChange} className="form-control" id="area" rows="8"></textarea>
+                    <textarea value={text} style={{backgroundColor:props.mode==='light'?'#f8f9fa':'#495057', color: props.mode==='light'?'black':'white'}} onChange={handleOnChange} className="form-control" id="area" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>To Upper Case</button>
-                <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>To Lower Case</button>
-                <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy text</button>
-                <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                <button className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleUpClick}>To Upper Case</button>
+                <button className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleLoClick}>To Lower Case</button>
+                <button className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleCopy}>Copy text</button>
+                <button className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                <button className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleClear}>Clear Text</button>
             </div>
-            <div className="container">
+            <div className="container" style={{color: props.mode==='light'?'black':'white'}}>
                 <h2>Number of words: </h2>
                 {text.split(' ').length}
                 <h2>Number of characters: </h2>
